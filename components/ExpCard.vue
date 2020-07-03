787d7638ee89fabec5e6fc7a8ab5ee9c3ba4c709
Anik Das <anikdas0811@gmail.com>
2020-07-03 17:32:26 +0530

    + Fixed image alt texts

--- components/ExpCard.vue
+++ components/ExpCard.vue
@@ -2,7 +2,7 @@
   <section class="text-gray-700 body-font">
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-        <img class="object-cover object-center rounded" alt="hero" :src="expImgSrc">
+        <img class="object-cover object-center rounded" alt="Project Related Image" :src="expImgSrc">
       </div>
       <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
         <h1 class="title-font sm:text-4xl text-3xl mb-8 font-medium text-gray-900">

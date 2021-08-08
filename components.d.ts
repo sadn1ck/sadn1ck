770996523d17aa504ad726cc531b7269f43fc18c
Anik Das <anikdas0811@gmail.com>
2021-08-08 17:53:09 +0530

    fix(ssg): fix ssg build
    pin vueuse/head to 0.5.1, might upgrade later
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- components.d.ts
+++ components.d.ts
@@ -3,28 +3,11 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    BiX: typeof import('virtual:vite-icons/bi/x')['default']
-    CarbonCampsite: typeof import('virtual:vite-icons/carbon/campsite')['default']
-    CarbonDicomOverlay: typeof import('virtual:vite-icons/carbon/dicom-overlay')['default']
-    CarbonLanguage: typeof import('virtual:vite-icons/carbon/language')['default']
-    CarbonLogoGithub: typeof import('virtual:vite-icons/carbon/logo-github')['default']
-    CarbonMoon: typeof import('virtual:vite-icons/carbon/moon')['default']
-    CarbonPedestrian: typeof import('virtual:vite-icons/carbon/pedestrian')['default']
-    CarbonSun: typeof import('virtual:vite-icons/carbon/sun')['default']
     CarbonWarning: typeof import('virtual:vite-icons/carbon/warning')['default']
-    CiHamburger: typeof import('virtual:vite-icons/ci/hamburger')['default']
-    FeatherTwitter: typeof import('virtual:vite-icons/feather/twitter')['default']
     Footer: typeof import('./src/components/Footer.vue')['default']
-    LaRssSquare: typeof import('virtual:vite-icons/la/rss-square')['default']
     Navbar: typeof import('./src/components/Navbar.vue')['default']
-    README: typeof import('./src/components/README.md')['default']
-    RiArticleLine: typeof import('virtual:vite-icons/ri/article-line')['default']
-    RiBookmarkLine: typeof import('virtual:vite-icons/ri/bookmark-line')['default']
-    RiLightbulbLine: typeof import('virtual:vite-icons/ri/lightbulb-line')['default']
     RiMoonLine: typeof import('virtual:vite-icons/ri/moon-line')['default']
-    RiStickyNoteLine: typeof import('virtual:vite-icons/ri/sticky-note-line')['default']
     RiSunLine: typeof import('virtual:vite-icons/ri/sun-line')['default']
-    UilGithubAlt: typeof import('virtual:vite-icons/uil/github-alt')['default']
   }
 }
 

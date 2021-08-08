b750b4425893e8528a857e8a5d6de9c758df55ed
Anik Das <anikdas0811@gmail.com>
2021-08-08 17:25:13 +0530

    chore: disabled ssg, broken
    need to debug, failing somewhere around @vueuse/head when inserting meta
    during server side generation
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<script setup lang="ts">
-import { useHead } from '@vueuse/head'
-useHead({
-  meta: [
-    { property: 'og:title', content: 'Anik Das' },
-    { property: 'og:image', content: 'https://anik.live/anik.jpg' },
-    { name: 'description', content: 'My Personal Website' },
-    { name: 'twitter:card', content: 'summary' },
-    { name: 'twitter:creator', content: '@s02n1ck' },
-  ],
-})
-</script>

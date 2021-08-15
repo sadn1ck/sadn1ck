8be0762d98bd707d5c70e8d5650fbf1caeee5486
Anik Das <anikdas0811@gmail.com>
2021-08-15 21:59:54 +0530

    feat(resume): setup resume redirect
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ src/pages/resume.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="text-center">
+    <a class="text-xl" :href="RESUME_URL">Redirecting. If stuck, click here</a>
+  </div>
+</template>
+
+<script setup lang="ts">
+const RESUME_URL = 'https://drive.google.com/file/d/11RewrnkZ4WVkDOFusuqm-wCPPJjGt044/view'
+onMounted(() => {
+  if (location)
+    location.replace(RESUME_URL)
+})
+</script>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>

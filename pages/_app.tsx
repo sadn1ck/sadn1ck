e718d54eff1ad34deff0f7d2d6f6e957826fb11d
Anik Das <anikdas0811@gmail.com>
2022-04-30 14:10:27 +0530

    feat: umami analytics via railway :+1:
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,22 @@ import { AnimatePresence } from "framer-motion";
 import Navbar from "components/Navbar";
 import Footer from "components/Footer";
 import "../styles/globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps, router }) {
   const url = `https://wallis.dev${router.route}`;
   return (
     <>
+      <Head>
+        <Script
+          async
+          defer
+          data-website-id="bb4be3ad-5765-44e0-b6c3-9ba7a9a89a92"
+          data-do-not-track="true"
+          src="https://analytics.anikd.com/umami.js"
+        ></Script>
+      </Head>
       <Navbar />
       <AnimatePresence
         exitBeforeEnter={true}

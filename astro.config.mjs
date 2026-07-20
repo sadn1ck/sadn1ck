aa798dd5b0b1e9c2c8b20aba1e0c4782e5e4d207
Anik Das <anikdas0811@gmail.com>
2026-07-20 08:56:51 +0530

    Simplify personal site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,12 @@
-import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://anikd.com",
-  integrations: [
-    tailwind({
-      nesting: true,
-    }),
-    sitemap(),
-    mdx(),
-    pagefind(),
-  ],
+  integrations: [sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       theme: "css-variables",
